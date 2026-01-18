f09041ff789730c26309aefac7889d39edf99df6
Ticoloi <130762407+Ticoloi@users.noreply.github.com>
2026-01-18 16:28:22 +0100

    Refactor Sudoku class and remove unused methods

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -2,219 +2,130 @@
  * Ticoloi
  * 2026
  */
-#include "sudoku.h"
+#ifndef SUDOKU_H
+#define SUDOKU_H
 
-map<int, bool> Sudoku::get_empty_dict() const
-{
-    map<int,bool> dict;
-    for(int n = 1; n <= this->get_size(); n++){
-        dict.insert(pair<int,bool>(n, false));
-    }
-    return dict;
-}
+#include <iostream>
+#include <vector>
+#include <vector2i.h>
+#include <fstream>
+#include <map>
 
+#include "data.h"
 
-void Sudoku::build_sudoku(fstream& file)
-{   
-    // buildding an empty table
-    for(int i = 0; i < this->size; i++){
-        vector<Data> n;
-        for(int j = 0; j < this->size; j++){
-            n.push_back(Data(0, false));
-        }
-        table.push_back(n);
-    }
+using namespace std;
 
-    // fillin the table
-    int n = 0;
-    while (!file.eof())
-    {
-       string line;
-       getline(file, line);
-       for(int i = 0; i < line.size(); i++){
-            if(line[i] != '0'){
-                this->table[n][i] = Data((int)line[i]  - 48, true);
-            }
-       }
-       n++;
-    }
-    // file closes here
-    file.close();
-}
+/**
+ * This object stores the sudoku information and
+ * manages logic.
+*/
+class Sudoku{
+    private:
 
-Sudoku::Sudoku(const int &size, fstream& file)
-{
-    this->size = size*size;
-    this->build_sudoku(file);
-}
+    /**
+     * returns a dictionary with numbers,
+     * its used in is_valid function
+     */
+    map<int, bool> get_empty_dict() const;
 
-Sudoku::Sudoku(fstream& file)
-{
-    this->size = 9;
-    this->build_sudoku(file);
-}
+    /** 
+    * Here is where sudoku stores
+    * numbers put by user and initials
+    */
+    vector<vector<Data>> table;
 
-int Sudoku::get_tile(const Vector2i &pos) const
-{
-    return this->table[pos.x][pos.y].get_number();
-}
-
-bool Sudoku::can_be_edited(const Vector2i &pos) const
-{
-    return !this->table[pos.x][pos.y].is_constant();
-}
-
-int Sudoku::get_size() const
-{
-    return size;
-}
-
-int Sudoku::get_sub_size() const
-{
-    return sqrt(size);
-}
-
-bool Sudoku::is_valid() const
-{
-    Vector2i coords;
-    for(int i = 0 ; i < this->get_size(); i++){
-        // Create 2 dictionaries
-        map<int, bool> dict_vert = this->get_empty_dict();
-        map<int, bool> dict_hor = this->get_empty_dict();
-        // now let's check rows
-        for(int j = 0 ; j < this->get_size(); j++){
-            // do a check (horizontal)
-            coords = Vector2i(i,j);
-            if(this->table[coords.x][coords.y].get_number() != 0){
-                if(dict_hor[this->table[coords.x][coords.y].get_number()]){
-                    return false;
-                }else{
-                    dict_hor[this->table[coords.x][coords.y].get_number()] = true;
-                }
-            }
-            // another check (vertical)
-            coords = Vector2i(j,i);
-            if(this->table[coords.x][coords.y].get_number() != 0){
-                if(dict_vert[this->table[coords.x][coords.y].get_number()]){
-                    return false;
-                }else{
-                    dict_vert[this->table[coords.x][coords.y].get_number()] = true;
-                }
-            }
-        }
-    }
-    // I may be wrong abaut vertical and horizontal, sorry
-    // Ok, now let's check other area
-    const int QUADRANT_SIZE = this->get_sub_size();
-    for(int y = 0; y < QUADRANT_SIZE; y++){
-        for(int x = 0; x < QUADRANT_SIZE; x++){
-            // create a new dictionary
-            map<int, bool> dict = get_empty_dict();
-            // iterate AGAIN
-            for(int i = 0; i < QUADRANT_SIZE; i++){
-                for(int j = 0; j < QUADRANT_SIZE; j++){
-                    coords = Vector2i(
-                        x * QUADRANT_SIZE + j,
-                        y * QUADRANT_SIZE + i
-                    );
-                    int number =  this->table[coords.x][coords.y].get_number();
-                    if(number != 0){
-                        if(dict[number]){
-                            return false;
-                        }else{
-                            dict[number] = true;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return true;
-}
-
-bool Sudoku::is_completed() const
-{
-    for(int i = 0;  i < this->get_size(); i++){
-        for(int j = 0;  j < this->get_size(); j++){
-            if(this->table[j][i].get_number() == 0){
-                return false;
-            }
-        }
-    }
-    return is_valid();
-}
-
-bool Sudoku::add_tile(const Vector2i &pos)
-{
-    if(this->table[pos.x][pos.y].is_constant()){
-        return false;
-    }
-    return true;
-}
-
-Data *Sudoku::operator[](const Vector2i &pos)
-{
-    return &this->table[pos.x][pos.y];
-}
-
-Data Sudoku::operator[](const Vector2i &pos) const
-{
-    return this->table[pos.x][pos.y];
-}
+    /**  
+    * The size of sudoku, multiply this size*size
+    */
+    int size;
+    /*
+        The sqaure root of size. (3*3 has size of 9)
+    */
+    int sub_size;
 
 
-vector<Data> *Sudoku::operator[](const int &pos)
-{
-     return &this->table[pos];
-}
+    // PRIVATE METHODS
 
-vector<Data> Sudoku::operator[](const int &pos) const
-{
-    return this->table[pos];
-}
+    /**
+     * Creates the sudoku, this functions is only 
+     * called from constructors methods
+     */
+    void build_sudoku(fstream& file);
 
-Sudoku &Sudoku::operator=(const Sudoku &s)
-{
-    this->table = s.table;
-    this->size = s.get_size();
-    return *this;
-}
+    public:
 
-ostream &operator<<(ostream &os, const Sudoku &s)
-{
-    // this function is not too easy to read, i know
-    for(int i = 0; i < s.get_size(); i++){
-        // ADDING - IN WHITE
-        if(i%int(sqrt(s.get_size())) == 0){
-            for(int j = 0 - int(sqrt(s.get_size()) + 1);  j < s.get_size(); j++){
-                for(int k = 0; k < SPACE ; k++){
-                    // LOOPING FOR ADDING '-'
-                    os << BOLDWHITE << "-" << RESET;
-                }
-                
-            }
-            os << endl;
-        }
-        for(int j = 0;  j < s.get_size(); j++){
-            // ADDING NUMBERS
-            if(j%int(sqrt(s.get_size())) == 0){
-                // SEPARATION
-                os << BOLDWHITE << setw(SPACE)  << "|" << RESET;
-            }
-            os << s[Vector2i(i,j)];
-        }
-        // the end of row
-        os << BOLDWHITE << setw(SPACE)  << "|" << RESET;
-        os << endl;
-        
-    }
-    for(int j = 0 - int(sqrt(s.get_size()) + 1);  j < s.get_size(); j++){
-        // LOOPING FOR ADDING '-', this way you have the end
-        for(int k = 0; k < SPACE ; k++){
-            os << BOLDWHITE << "-" << RESET;
-        }
-    }
-    cout << endl;
-    return os;
-}
 
+    // CONSTRUCTORS
+
+    /**
+     * Create new sudoku
+     * whith size*size
+     * size*size x size*sixe
+     */
+    Sudoku(const int &size, fstream& file);
+
+    /**
+     * Create new sudoku
+     * whith size 9 x 9
+     */
+    Sudoku(fstream& file);
+
+
+    // GETTERS
+
+    int get_tile(const Vector2i& pos) const;
+
+    /**
+     * True if can be edited
+     * false if not
+     */
+    bool can_be_edited(const Vector2i& pos) const;
+
+    /**
+    * returns size of sudoku
+    */
+    int get_size() const;
+
+    /**
+    * returns square rot of size, lol
+    */
+    int get_sub_size() const;
+
+    /*
+        returns if the soultion is valid or not
+    */
+    bool is_valid() const;
+
+    /**
+     * returns true if is completed
+     */
+    bool is_completed() const;
+
+    // SETTERS
+
+    /**
+    * If possible modifiy the tile on that position
+    * if not possible returns false
+    */
+    bool add_tile(const Vector2i& pos);
+
+    // OPERATORS
+
+    Data* operator[](const Vector2i& pos);
+
+    Data operator[](const Vector2i& pos) const;
+
+    vector<Data>* operator[](const int& pos);
+
+    vector<Data> operator[](const int& pos) const;
+
+    Sudoku& operator=(const Sudoku &s); 
+
+    // COUT
+
+    friend ostream& operator<<(ostream& os, const Sudoku& s);
+
+
+};
+
+#endif // SUDOKU_H
